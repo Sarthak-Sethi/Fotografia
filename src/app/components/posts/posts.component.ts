4d7d8ea47077462cb0b81f5f8a5964506d9f47bf
Sarthak-Sethi <sarthaksethigithub@gmail.com>
2021-09-02 01:10:27 +0530

    home, user and pos component

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  post ;
+  upvote = 0;
+  instaId = null;
+  uid = null;
+  
+  likeimg = "../../../assets/like.png";
+
+  constructor(private db : AngularFireDatabase, private auth : AuthService) {
+    auth.getUser().subscribe((user)=> {this.uid = user?.uid});
+   }
 
   ngOnInit(): void {
   }
 
+  upvotePost(){
+    this.db.object(`/posts/${this.uid}/vote/${this.uid}`).set({ upvote : this.upvote+1});
+  }
+  getInstaUrl(){
+    console.log(this.post?.instaId);
+    this.instaId = "https://www.instagram.com/"+this.post.instaId;
+  }
+
+  fillit(){
+     this.likeimg === "../../../assets/like.png" ? this.likeimg = "../../../assets/like_filled.png" : this.likeimg = "../../../assets/like.png"
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ToastService } from 'angular-toastify';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  users = [];
+  posts =[];
+  isLoading : boolean = false;
+  constructor(private db :AngularFireDatabase,private toast : ToastService) {
+    this.isLoading = true;
+    // grab all users
+    db.object('/users')
+    .valueChanges()
+    .subscribe((obj) => {
+      obj ? this.users = Object.values(obj) : toast.error("no user found");
+    });
 
-  constructor() { }
+    // grab all posts
+    db.object('/posts')
+    .valueChanges()
+    .subscribe((obj) => {
+      if(obj) {
+       this.posts = Object.values(obj).sort((d1,d2) => d2.date - d1.date );
+       this.isLoading = false;
+      }
+      else {
+        toast.error("No post here .. !");
+        this.isLoading = false;
+      }
+      
+    });
+
+   }
 
   ngOnInit(): void {
   }
